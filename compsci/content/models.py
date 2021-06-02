8cffc94edf535bfb022106fca4f9c1752e29a4e3
Hugh Ellison <hughellison@gmail.com>
2021-06-02 23:10:58 +0100

    Added contents model for index page content. Issue with static files too still be sorted.

--- compsci/content/models.py
+++ compsci/content/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class HomePageTopPost(models.Model):
+    title = models.TextField()
+    message = models.TextField()
+    create_at = models.DateTimeField(auto_now = True)
+    upload = models.ImageField(upload_to ='uploads/')
+    Bg_Colour = models.TextField()
