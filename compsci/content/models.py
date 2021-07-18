58914a3be98080cd24da72315bfd46253e0f7b3e
Hugh Ellison <hughellison@gmail.com>
2021-07-18 22:11:01 +0100

    Updated Homepage with content.

--- compsci/content/models.py
+++ compsci/content/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 class HomePageTopPost(models.Model):
     title = models.TextField()
     message = models.TextField()
+    link = models.URLField(blank = True)
     create_at = models.DateTimeField(auto_now = True)
     upload = models.ImageField(upload_to ='static/content/', blank = False)
     Bg_Colour = models.TextField()
